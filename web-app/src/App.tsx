15a241d879736dfd5316ac04f713636267d8e92f
WLeskinen <58485176+WLeskinen@users.noreply.github.com>
2024-03-06 16:12:12 +0200

    Webpage with UI and temporary values added.

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import TemperatureDisplay from './TemperatureDisplay';
+import WeatherBox from './WeatherBox';
 
 const App: React.FC = () => {
+  // Fetch the weather data here...
+  const temperature = '22.00'; // Replace with actual data later
+  const humidity = '78.00'; // Replace this with data later
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Real-Time Temperature Monitor</h1>
+        <h1>WeatherOrNot</h1>
+        <h3>Current Location</h3>
+        <WeatherBox temperature={temperature} humidity={humidity} />
+        <h3>Mikkeli</h3>
+        <WeatherBox temperature={temperature} humidity={humidity} />
       </header>
-      <main>
-        <TemperatureDisplay />
-      </main>
     </div>
   );
 };
---
+++ web-app/src/WeatherBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WeatherBox = ({ temperature, humidity }) => (
+  <div className="box">
+    <p>
+      <h2>Average Temperature</h2>
+      <span>{temperature}°C</span>
+      <br />
+      <h2>Humidity:</h2> 
+      <span>{humidity}%</span>
+    </p>
+  </div>
+);
+
+export default WeatherBox;
